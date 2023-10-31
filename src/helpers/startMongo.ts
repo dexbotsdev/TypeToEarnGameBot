4722e0f201fffe9d194ff0a283b29fdf8241476b
DefiCody <ambergridpro@gmail.com>
2023-10-31 22:14:39 +0530

    db

--- src/helpers/startMongo.ts
+++ src/helpers/startMongo.ts
@@ -1,8 +1,12 @@
 import mongoose, { connect } from 'mongoose'
 import env from '@/helpers/env'
 
+const username = encodeURIComponent("dexbotdev");
+const password = encodeURIComponent("Orangefire#414");
+const uri = `mongodb+srv://${username}:${password}@cluster0.n6sebl1.mongodb.net/?retryWrites=true&w=majority`
+
 function startMongo() {
-  return connect(env.MONGO)
+  return connect(uri)
 }
 mongoose.set('strictQuery', false)
 export default startMongo
