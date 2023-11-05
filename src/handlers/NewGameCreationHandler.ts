16da602aa0710313e98245fb04370178524df32c
DefiCody <ambergridpro@gmail.com>
2023-11-05 23:48:54 +0530

    w

--- src/handlers/NewGameCreationHandler.ts
+++ src/handlers/NewGameCreationHandler.ts
@@ -73,7 +73,8 @@ Prize Pool non-holders : $100
 
 
                 try {
-                    await joinGame(createGamenew.gameId, 'oxdefimermaid', word, randomInt(18, 35), randomInt(18, 35), randomInt(145, 400));
+                    // await joinGame(createGamenew.gameId, 'oxdefimermaid', word, randomInt(18, 35), randomInt(18, 35), randomInt(145, 400));
+                    console.log('animal already played')
                 } catch (err) {
                     console.log('animal already played')
                 }
